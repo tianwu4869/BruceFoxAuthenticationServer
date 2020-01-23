107c79961333d5f677c363ea07ea4b6301ec9d8e
Tian <wu000130@umn.edu>
2020-01-23 14:48:06 -0500

    first commit

---
+++ BruceFoxAuthenticationServer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace BruceFoxAuthenticationServer.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ BruceFoxAuthenticationServer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BruceFoxAuthenticationServer.WebApiApplication" Language="C#" %>
